83bcaa9487d67a0485be936a1237c1761e38d2a8
MrPatcher <eljakaniyassin@gmail.com>
2024-10-28 09:15:49 +0100

    initialisation de projet

---
+++ src/test/java/com/devsecops/ensa/employee/EmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsecops.ensa.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
